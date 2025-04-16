3b73e856e1b2f6884e40a56c3daf305481d67041
Ubuntu <azureuser@A100-train.411rxyaovxtejils41i42wghoh.cx.internal.cloudapp.net>
2025-04-16 06:52:17 +0000

    dapo script for single gpu

---
+++ src/open-r1-multimodal/single_dapo.sh
@@ -0,0 +1,22 @@
+python src/open_r1/grpo.py \
+    --output_dir checkpoints/testQwenRun \
+    --model_name_or_path Qwen/Qwen2.5-VL-3B-Instruct \
+    --dataset_name leonardPKU/clevr_cogen_a_train \
+    --max_prompt_length 512 \
+    --max_completion_length 512 \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 8 \
+    --logging_steps 1 \
+    --bf16 \
+    --report_to wandb \
+    --gradient_checkpointing true \
+    --attn_implementation flash_attention_2 \
+    --max_pixels 401408 \
+    --num_train_epochs 1 \
+    --run_name qwen-test-on-clevr \
+    --save_steps 100 \
+    --save_only_model true \
+    --num_generations 2 \
+    --epsilon_low 0.20 \
+    --epsilon_high 0.28 \
+    --optim "adamw_torch_fused"
