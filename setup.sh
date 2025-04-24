789335f6b1da6755b6c756ae4914f8c2e8ccfca6
KevinDayve <kevindave5735@gmail.com>
2025-04-24 17:35:51 +0530

    .

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ pip install wandb==0.18.3
 pip install tensorboardx
 pip install qwen_vl_utils torchvision
 pip install flash-attn --no-build-isolation
+pip install peft==0.15.2
 
 # vLLM support
 pip install vllm==0.7.2
