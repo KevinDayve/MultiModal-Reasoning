4e83b0e531f66d370f064d30abc1368909274aca
Kevin Dave <kevindave5735@gmail.com>
2025-06-17 15:01:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ pip install wandb==0.18.3
 pip install tensorboardx
 pip install qwen_vl_utils torchvision
 pip install flash-attn --no-build-isolation
+pip install openai
 
 # vLLM support 
 pip install vllm==0.7.2
